bd460a3df954b872ee5282f6eb0d1177b6d411d1
qolvap <pgrebennikow@yahoo.pl>
2023-11-09 18:01:31 +0100

    ✅ updated styles & data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const people = [
+    {
+      id: 1,
+      image: 'https://thispersondoesnotexist.com/',
+      name: 'maria ferguson',
+      title: 'office manager',
+      quote:
+        'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+    },
+    {
+      id: 2,
+      image: 'https://images.squarespace-cdn.com/content/v1/5dd366b5ac1101724ff2fac7/1574238281417-HMZE4HR3YF5SC6U7IRX4/man+1.jpg?format=750w',
+      name: 'john doe',
+      title: 'regular guy',
+      quote:
+        'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+    },
+    {
+      id: 3,
+      image: 'https://this-person-does-not-exist.com/img/avatar-gen111b6112cb740a683fb73169a53ab9c3.jpg',
+      name: 'peter smith',
+      title: 'product designer',
+      quote:
+        'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+    },
+    {
+      id: 4,
+      image: 'https://cdnstorage.sendbig.com/unreal/female.webp',
+      name: 'susan andersen',
+      title: 'the boss',
+      quote:
+        'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+    },
+  ];
+  
+  export default people;
